9707521cf0cba7903f69757217ebe2a84164848d
Braian A. Diez <bdiez19@gmail.com>
2025-01-29 15:53:15 -0300

    chore: improving netlify build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: [
-    "@unocss/nuxt",
-    "@vueuse/nuxt",
-    "@nuxtjs/sitemap",
-    "@nuxt/content",
-    "@nuxtjs/fontaine",
-    "@nuxtjs/color-mode",
-  ],
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-    }
-  },
-  css: ["@unocss/reset/tailwind.css"],
-  content: {
-    build: {
-      markdown: {
-        toc: { depth: 2 },
-        highlight: {
-          langs: ["bash", "ts", "html", "css", "nix", "vue", "rust", "js", "tsx", "jsx", "zsh", "nushell", "md", "mdx"],
-          theme: {
-            default: 'kanagawa-lotus',
-            dark: 'kanagawa-wave',
-          }
-        }
-      },
-    },
-  },
-  colorMode: {
-    preference: "system",
-    classSuffix: "",
-  },
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	modules: [
+		"@unocss/nuxt",
+		"@vueuse/nuxt",
+		"@nuxtjs/sitemap",
+		"@nuxt/content",
+		"@nuxtjs/fontaine",
+		"@nuxtjs/color-mode",
+	],
+	app: {
+		head: {
+			charset: "utf-8",
+			viewport: "width=device-width, initial-scale=1",
+		},
+	},
+	css: ["@unocss/reset/tailwind.css"],
+	content: {
+		database: {
+			type: "postgres",
+			url: process.env.POSTGRES_URL,
+		},
+		build: {
+			markdown: {
+				toc: { depth: 2 },
+				highlight: {
+					langs: [
+						"bash",
+						"ts",
+						"html",
+						"css",
+						"nix",
+						"vue",
+						"rust",
+						"js",
+						"tsx",
+						"jsx",
+						"zsh",
+						"nushell",
+						"md",
+						"mdx",
+					],
+					theme: {
+						default: "kanagawa-lotus",
+						dark: "kanagawa-wave",
+					},
+				},
+			},
+		},
+	},
+	colorMode: {
+		preference: "system",
+		classSuffix: "",
+	},
+	routeRules: {
+		"/": { prerender: true },
+	},
 });
